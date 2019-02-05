5bdee698af93117b047673a4b350cbfc1f98ce94
Milad Doorbash <milad.doorbash@gmail.com>
2019-02-06 03:08:16 +0330

    now players can eat fruits

--- client/core/src/ir/doorbash/agar_io_clone/Game.java
+++ client/core/src/ir/doorbash/agar_io_clone/Game.java
@@ -15,11 +15,27 @@ import io.colyseus.state_listener.FallbackPatchListenerCallback;
 import io.colyseus.state_listener.PatchListenerCallback;
 import io.colyseus.state_listener.PatchObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 public class Game extends ApplicationAdapter {
 
+    class Player {
+        public float x;
+        public float y;
+        public Color color;
+        public Color strokeColor;
+        public float radius;
+    }
+
+    class Fruit {
+        public float x;
+        public float y;
+        public Color color;
+        boolean eaten;
+    }
 
     private ShapeRenderer shapeRenderer;
     private OrthographicCamera camera;
@@ -29,16 +45,9 @@ public class Game extends ApplicationAdapter {
     Client client;
     Room room;
     double lastAngle = -1000;
-
-    class Player {
-        public float x;
-        public float y;
-        public Color color;
-        public float radius;
-    }
-
     HashMap<String, Player> players = new HashMap<>();
-
+    Fruit[] fruits = new Fruit[10000];
+    int numFruits = 0;
 
     @Override
     public void create() {
@@ -62,6 +71,7 @@ public class Game extends ApplicationAdapter {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(new Color(0, 0, 0, 0.1f));
         drawGrid(-300, -300, (int) gridSize.x, (int) gridSize.y, 60);
+        drawFruits();
         drawPlayers();
         shapeRenderer.end();
 
@@ -71,6 +81,8 @@ public class Game extends ApplicationAdapter {
     @Override
     public void dispose() {
         shapeRenderer.dispose();
+        if (room != null) room.leave();
+        if (client != null) client.close();
     }
 
     @Override
@@ -87,26 +99,35 @@ public class Game extends ApplicationAdapter {
         }
     }
 
+    private void drawFruits() {
+        for (int i = 0; i < numFruits; i++) {
+            Fruit fr = fruits[i];
+            if (fr.eaten) continue;
+            shapeRenderer.setColor(fr.color);
+            shapeRenderer.circle(fr.x, fr.y, 10);
+        }
+    }
+
     private void drawPlayers() {
 
-        if(client == null) return;
+        if (client == null) return;
 
         // draw other players
         for (String id : players.keySet()) {
             if (id.equals(client.getId())) continue;
             Player player = players.get(id);
-            shapeRenderer.setColor(Color.BLACK);
+            shapeRenderer.setColor(player.strokeColor);
             shapeRenderer.circle(player.x, player.y, player.radius);
             shapeRenderer.setColor(player.color);
-            shapeRenderer.circle(player.x, player.y, player.radius - 1);
+            shapeRenderer.circle(player.x, player.y, player.radius - 3);
         }
         // draw me
         Player player = players.get(client.getId());
-        if(player == null) return;
-        shapeRenderer.setColor(Color.BLACK);
+        if (player == null) return;
+        shapeRenderer.setColor(player.strokeColor);
         shapeRenderer.circle(player.x, player.y, player.radius);
         shapeRenderer.setColor(player.color);
-        shapeRenderer.circle(player.x, player.y, player.radius - 1);
+        shapeRenderer.circle(player.x, player.y, player.radius - 3);
     }
 
     private void adjustCamera() {
@@ -126,10 +147,10 @@ public class Game extends ApplicationAdapter {
                 room.addPatchListener("players/:id", new PatchListenerCallback() {
                     @Override
                     protected void callback(DataChange dataChange) {
-                        System.out.println(">>> players/:id");
-                        System.out.println(dataChange.path);
-                        System.out.println(dataChange.operation);
-                        System.out.println(dataChange.value);
+//                        System.out.println(">>> players/:id");
+//                        System.out.println(dataChange.path);
+//                        System.out.println(dataChange.operation);
+//                        System.out.println(dataChange.value);
                         if (dataChange.operation.equals("add")) {
                             Player player = new Player();
                             LinkedHashMap<String, Object> data = (LinkedHashMap<String, Object>) dataChange.value;
@@ -150,8 +171,12 @@ public class Game extends ApplicationAdapter {
                             else if (data.get("radius") instanceof Integer)
                                 player.radius = ((Integer) data.get("radius")).floatValue();
 
-                            int color = (Integer) data.get("color");
+                            int color;
+                            if (data.get("color") instanceof Long) color = ((Long) data.get("color")).intValue();
+                            else color = (Integer) data.get("color");
                             player.color = new Color(color);
+                            player.strokeColor = new Color(player.color);
+                            player.strokeColor.mul(0.9f);
 
                             players.put(dataChange.path.get("id"), player);
                         } else if (dataChange.operation.equals("remove")) {
@@ -186,17 +211,62 @@ public class Game extends ApplicationAdapter {
                             } else if (dataChange.value instanceof Float) {
                                 player.y = ((Float) dataChange.value).floatValue();
                             }
+                        } else if (attribute.equals("radius")) {
+                            if (dataChange.value instanceof Float) player.radius = (Float) dataChange.value;
+                            else if (dataChange.value instanceof Double)
+                                player.radius = ((Double) dataChange.value).floatValue();
+                            else if (dataChange.value instanceof Integer)
+                                player.radius = ((Integer) dataChange.value).floatValue();
                         }
 
+                    }
+                });
+                room.addPatchListener("fruits/:id", new PatchListenerCallback() {
+                    @Override
+                    protected void callback(DataChange dataChange) {
+                        System.out.println(dataChange);
+                        if (dataChange.operation.equals("add")) {
+                            Fruit fruit = new Fruit();
+
+                            LinkedHashMap<String, Object> data = (LinkedHashMap<String, Object>) dataChange.value;
+
+                            if (data.get("x") instanceof Float) fruit.x = (Float) data.get("x");
+                            else if (data.get("x") instanceof Double) fruit.x = ((Double) data.get("x")).floatValue();
+                            else if (data.get("x") instanceof Integer)
+                                fruit.x = ((Integer) data.get("x")).floatValue();
+
+                            if (data.get("y") instanceof Float) fruit.y = (Float) data.get("y");
+                            else if (data.get("y") instanceof Double) fruit.y = ((Double) data.get("y")).floatValue();
+                            else if (data.get("y") instanceof Integer)
+                                fruit.y = ((Integer) data.get("y")).floatValue();
+
+                            int color;
+                            if (data.get("color") instanceof Long) color = ((Long) data.get("color")).intValue();
+                            else color = (Integer) data.get("color");
+                            fruit.color = new Color(color);
+
+                            fruit.eaten = (boolean) data.get("eaten");
+
+                            fruits[Integer.parseInt(dataChange.path.get("id"))] = fruit;
+                            numFruits++;
+                        }
+                    }
+                });
+                room.addPatchListener("fruits/:id/:attribute", new PatchListenerCallback() {
+                    @Override
+                    protected void callback(DataChange dataChange) {
+                        if (!dataChange.operation.equals("replace")) return;
+                        if (!dataChange.path.get("attribute").equals("eaten")) return;
+                        fruits[Integer.parseInt(dataChange.path.get("id"))].eaten = true;
                     }
                 });
                 room.setDefaultPatchListener(new FallbackPatchListenerCallback() {
                     @Override
                     public void callback(PatchObject patchObject) {
-                        System.out.println(" >>> default listener");
-                        System.out.println(patchObject.path);
-                        System.out.println(patchObject.operation);
-                        System.out.println(patchObject.value);
+//                        System.out.println(" >>> default listener");
+//                        System.out.println(patchObject.path);
+//                        System.out.println(patchObject.operation);
+//                        System.out.println(patchObject.value);
                     }
                 });
             }
@@ -222,7 +292,7 @@ public class Game extends ApplicationAdapter {
         if (room == null) return;
         float dx = Gdx.input.getX() - width / 2;
         float dy = height / 2 - Gdx.input.getY();
-        double angle = Math.atan2(dy, dx);
+        long angle = Math.round(Math.atan2(dy, dx) * 57.2958f);
         if (lastAngle != angle) {
             LinkedHashMap<String, Object> data = new LinkedHashMap<String, Object>();
             data.put("op", "angle");
--- server/src/rooms/public.ts
+++ server/src/rooms/public.ts
@@ -1,29 +1,49 @@
 import { Room, Presence, nosync } from "colyseus";
 
-const COLORS: number[] = [
-    0x4cb050,
-    0xe6194b, 0x3cb44b, 0xffe119, 0x4363d8,
-    0xf58231, 0x911eb4, 0x46f0f0, 0xf032e6,
-    0xbcf60c, 0xfabebe, 0x008080, 0xe6beff,
-    0x9a6324, 0xfffac8, 0x800000, 0xaaffc3,
+const PLAYER_COLORS: number[] = [
+    0x4cb050FF,
+    0xe6194bFF, 0x3cb44bFF, 0xffe119FF, 0x4363d8FF,
+    0xf58231FF, 0x911eb4FF, 0x46f0f0FF, 0xf032e6FF,
+    0xbcf60cFF, 0xfabebeFF, 0x008080FF, 0xe6beffFF,
+    0x9a6324FF, 0xfffac8FF, 0x800000FF, 0xaaffc3FF,
+];
+
+const FRUIT_COLORS: number[] = [
+    0xFF0000FF,
+    0x00FF00FF,
+    0x0000FFFF
 ];
 
 const WORLD_UPDATE_INTERVAL = 16; // ms
+const FRUIT_RADIUS = 10;
+const EAT_RADIUS_CHANGE = 1;
+const EAT_SPEED_CHANGE = 1;
+const INIT_FRUITS = 10;
+const PLAYER_MIN_SPEED = 40;
+const PLAYER_INIT_SPEED = 120;
 
 export class Player {
     x: number = 0;
     y: number = 0;
     radius: number = 40;
     color: number;
-    @nosync speed: number = 100;
+    @nosync speed: number = PLAYER_INIT_SPEED;
     @nosync angle = Math.PI * (Math.random() * 2 - 1);
+}
+
+export class Fruit {
+    x: number;
+    y: number;
+    color: number;
+    eaten: boolean;
 }
 
 export class GameState {
     players = {};
     mapSize = {
-        width: 300, height: 300
+        width: 1000, height: 1000
     };
+    fruits = [];
 }
 
 export class PublicRoom extends Room<GameState> {
@@ -42,9 +62,12 @@ export class PublicRoom extends Room<GameState> {
     onInit(options) {
         console.log(this.getLogTag(), "Game Room created!", options);
 
-        var gameState = new GameState();
-        gameState.players = {};
-        this.setState(gameState);
+        var state = new GameState();
+        state.players = {};
+        this.setState(state);
+
+        for (var i = 0; i < INIT_FRUITS; i++)
+            this.generateFruit();
 
         this.setSimulationInterval(() => {
             // update world here
@@ -57,7 +80,7 @@ export class PublicRoom extends Room<GameState> {
         this.state.players[client.id] = new Player();
         this.state.players[client.id].x = Math.floor(Math.random() * this.state.mapSize.width);
         this.state.players[client.id].y = Math.floor(Math.random() * this.state.mapSize.height);
-        this.state.players[client.id].color = COLORS[Math.floor(Math.random() * COLORS.length)];
+        this.state.players[client.id].color = PLAYER_COLORS[Math.floor(Math.random() * PLAYER_COLORS.length)];
     }
 
     onLeave(client, consented?) {
@@ -70,7 +93,7 @@ export class PublicRoom extends Room<GameState> {
         var player = this.state.players[client.id];
         switch (data.op) {
             case 'angle': {
-                player.angle = data.angle;
+                player.angle = data.angle * 0.0174533;
             } break;
         }
     }
@@ -85,14 +108,47 @@ export class PublicRoom extends Room<GameState> {
 
     updateWorld() {
         Object.keys(this.state.players).forEach(key => {
+            // update player position
             var player = this.state.players[key];
             var newX = player.x + Math.cos(player.angle) * player.speed * WORLD_UPDATE_INTERVAL / 1000;
             var newY = player.y + Math.sin(player.angle) * player.speed * WORLD_UPDATE_INTERVAL / 1000;
-            if(newX < 0) newX = 0; else if(newX > this.state.mapSize.width) newX = this.state.mapSize.width;
-            if(newY < 0) newY = 0; else if(newY > this.state.mapSize.height) newY = this.state.mapSize.height;
+            if (newX < 0) newX = 0; else if (newX > this.state.mapSize.width) newX = this.state.mapSize.width;
+            if (newY < 0) newY = 0; else if (newY > this.state.mapSize.height) newY = this.state.mapSize.height;
             player.x = newX;
             player.y = newY;
+
+            // check if player eat something
+            this.checkIfPlayerIsEatingFruit(player);
         });
+    }
+
+    checkIfPlayerIsEatingFruit(player) {
+        this.state.fruits.forEach(fruit => {
+            if (fruit.eaten) return;
+            if ((fruit.x - FRUIT_RADIUS) > (player.x - player.radius) &&
+                (fruit.y - FRUIT_RADIUS) > (player.y - player.radius) &&
+                (fruit.x + FRUIT_RADIUS) < (player.x + player.radius) &&
+                (fruit.y + FRUIT_RADIUS) < (player.y + player.radius)) {
+                this.eat(player, fruit);
+            }
+        });
+    }
+
+    eat(player, fruit) {
+        fruit.eaten = true;
+        player.radius += EAT_RADIUS_CHANGE;
+        if (player.speed > PLAYER_MIN_SPEED) player.speed -= EAT_SPEED_CHANGE;
+        console.log('yum yum yummm');
+        this.generateFruit();
+    }
+
+    generateFruit() {
+        var fr: Fruit = new Fruit();
+        fr.x = Math.random() * this.state.mapSize.width;
+        fr.y = Math.random() * this.state.mapSize.height;
+        fr.color = FRUIT_COLORS[Math.floor(Math.random() * FRUIT_COLORS.length)];
+        fr.eaten = false;
+        this.state.fruits.push(fr);
     }
 
 }
