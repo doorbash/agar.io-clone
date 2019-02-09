2203e967dd333afe59371e5ebcf3229acc2c780b
Milad Doorbash <milad.doorbash@gmail.com>
2019-02-09 04:47:54 +0330

    lerp player position

--- client/core/src/ir/doorbash/agar_io_clone/Game.java
+++ client/core/src/ir/doorbash/agar_io_clone/Game.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import io.colyseus.Client;
 import io.colyseus.Room;
@@ -23,18 +24,18 @@ public class Game extends ApplicationAdapter {
     private static final int NETWORK_UPDATE_INTERVAL = 200;
     private static final int GRID_SIZE = 60;
     private static final int FRUIT_RADIUS = 10;
+    private static final float LERP_ALPHA = 0.5f;
 
     class Player {
-        float x;
-        float y;
+        Vector2 position = new Vector2();
+        Vector2 newPosition = new Vector2();
         Color color;
         Color strokeColor;
         float radius;
     }
 
     class Fruit {
-        float x;
-        float y;
+        Vector2 position = new Vector2();
         Color color;
     }
 
@@ -66,6 +67,7 @@ public class Game extends ApplicationAdapter {
     @Override
     public void render() {
 //        fpsLogger.log();
+        updatePositions();
         Gdx.gl.glClearColor(0.98f, 0.99f, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -100,6 +102,17 @@ public class Game extends ApplicationAdapter {
     public void resize(int width, int height) {
     }
 
+    private void updatePositions() {
+        if (client == null) return;
+        for (String clientId : players.keySet()) {
+            Player player = players.get(clientId);
+            if (player == null) continue;
+            if (clientId.equals(client.getId()))
+                player.position.lerp(player.newPosition, LERP_ALPHA);
+            else player.position = player.newPosition;
+        }
+    }
+
     private void drawGrid() {
         Player player;
         int leftBottomX;
@@ -108,8 +121,8 @@ public class Game extends ApplicationAdapter {
         int rightTopY;
 
         if (client != null && (player = players.get(client.getId())) != null) {
-            leftBottomX = (int) (player.x - width / 2);
-            leftBottomY = (int) (player.y - height / 2);
+            leftBottomX = (int) (player.position.x - width / 2);
+            leftBottomY = (int) (player.position.y - height / 2);
         } else {
             leftBottomX = -width / 2;
             leftBottomY = -height / 2;
@@ -145,9 +158,9 @@ public class Game extends ApplicationAdapter {
             for (String fruitId : fruits.keySet()) {
                 Fruit fr = fruits.get(fruitId);
                 if (fr == null) continue;
-                if (!objectIsInCurrentScreen(thisPlayer, fr.x, fr.y, FRUIT_RADIUS)) continue;
+                if (!objectIsInCurrentScreen(thisPlayer, fr.position.x, fr.position.y, FRUIT_RADIUS)) continue;
                 shapeRenderer.setColor(fr.color);
-                shapeRenderer.circle(fr.x, fr.y, FRUIT_RADIUS);
+                shapeRenderer.circle(fr.position.x, fr.position.y, FRUIT_RADIUS);
             }
         }
     }
@@ -159,20 +172,20 @@ public class Game extends ApplicationAdapter {
 
         for (String id : players.keySet()) {
             Player player = players.get(id);
-            if (id.equals(client.getId()) || objectIsInCurrentScreen(thisPlayer, player.x, player.y, player.radius)) {
+            if (id.equals(client.getId()) || objectIsInCurrentScreen(thisPlayer, player.position.x, player.position.y, player.radius)) {
                 shapeRenderer.setColor(player.strokeColor);
-                shapeRenderer.circle(player.x, player.y, player.radius);
+                shapeRenderer.circle(player.position.x, player.position.y, player.radius);
                 shapeRenderer.setColor(player.color);
-                shapeRenderer.circle(player.x, player.y, player.radius - 3);
+                shapeRenderer.circle(player.position.x, player.position.y, player.radius - 3);
             }
         }
     }
 
     private boolean objectIsInCurrentScreen(Player player, float x, float y, float radius) {
-        if (x + radius < player.x - width / 2) return false;
-        if (x - radius > player.x + width / 2) return false;
-        if (y + radius < player.y - height / 2) return false;
-        if (y - radius > player.y + height / 2) return false;
+        if (x + radius < player.position.x - width / 2) return false;
+        if (x - radius > player.position.x + width / 2) return false;
+        if (y + radius < player.position.y - height / 2) return false;
+        if (y - radius > player.position.y + height / 2) return false;
         return true;
     }
 
@@ -180,7 +193,7 @@ public class Game extends ApplicationAdapter {
         if (client == null) return;
         Player player = players.get(client.getId());
         if (player == null) return;
-        camera.position.set(new Vector3(player.x, player.y, 0));
+        camera.position.set(new Vector3(player.position.x, player.position.y, 0));
         camera.zoom = 1;
         camera.update();
     }
@@ -201,15 +214,17 @@ public class Game extends ApplicationAdapter {
                             Player player = new Player();
                             LinkedHashMap<String, Object> data = (LinkedHashMap<String, Object>) change.value;
 
-                            if (data.get("x") instanceof Float) player.x = (Float) data.get("x");
-                            else if (data.get("x") instanceof Double) player.x = ((Double) data.get("x")).floatValue();
+                            if (data.get("x") instanceof Float) player.position.x = (Float) data.get("x");
+                            else if (data.get("x") instanceof Double)
+                                player.position.x = ((Double) data.get("x")).floatValue();
                             else if (data.get("x") instanceof Integer)
-                                player.x = ((Integer) data.get("x")).floatValue();
+                                player.position.x = ((Integer) data.get("x")).floatValue();
 
-                            if (data.get("y") instanceof Float) player.y = (Float) data.get("y");
-                            else if (data.get("y") instanceof Double) player.y = ((Double) data.get("y")).floatValue();
+                            if (data.get("y") instanceof Float) player.position.y = (Float) data.get("y");
+                            else if (data.get("y") instanceof Double)
+                                player.position.y = ((Double) data.get("y")).floatValue();
                             else if (data.get("y") instanceof Integer)
-                                player.y = ((Integer) data.get("y")).floatValue();
+                                player.position.y = ((Integer) data.get("y")).floatValue();
 
                             if (data.get("radius") instanceof Float) player.radius = (Float) data.get("radius");
                             else if (data.get("radius") instanceof Double)
@@ -241,28 +256,32 @@ public class Game extends ApplicationAdapter {
                         Player player = players.get(change.path.get("id"));
                         if (player == null) return;
                         String attribute = change.path.get("attribute");
-                        if (attribute.equals("x")) {
-                            if (change.value instanceof Double) {
-                                player.x = ((Double) change.value).floatValue();
-                            } else if (change.value instanceof Integer) {
-                                player.x = ((Integer) change.value).floatValue();
-                            } else if (change.value instanceof Float) {
-                                player.x = (Float) change.value;
-                            }
-                        } else if (attribute.equals("y")) {
-                            if (change.value instanceof Double) {
-                                player.y = ((Double) change.value).floatValue();
-                            } else if (change.value instanceof Integer) {
-                                player.y = ((Integer) change.value).floatValue();
-                            } else if (change.value instanceof Float) {
-                                player.y = (Float) change.value;
-                            }
-                        } else if (attribute.equals("radius")) {
-                            if (change.value instanceof Float) player.radius = (Float) change.value;
-                            else if (change.value instanceof Double)
-                                player.radius = ((Double) change.value).floatValue();
-                            else if (change.value instanceof Integer)
-                                player.radius = ((Integer) change.value).floatValue();
+                        switch (attribute) {
+                            case "x":
+                                if (change.value instanceof Double) {
+                                    player.newPosition.x = ((Double) change.value).floatValue();
+                                } else if (change.value instanceof Integer) {
+                                    player.newPosition.x = ((Integer) change.value).floatValue();
+                                } else if (change.value instanceof Float) {
+                                    player.newPosition.x = (Float) change.value;
+                                }
+                                break;
+                            case "y":
+                                if (change.value instanceof Double) {
+                                    player.newPosition.y = ((Double) change.value).floatValue();
+                                } else if (change.value instanceof Integer) {
+                                    player.newPosition.y = ((Integer) change.value).floatValue();
+                                } else if (change.value instanceof Float) {
+                                    player.newPosition.y = (Float) change.value;
+                                }
+                                break;
+                            case "radius":
+                                if (change.value instanceof Float) player.radius = (Float) change.value;
+                                else if (change.value instanceof Double)
+                                    player.radius = ((Double) change.value).floatValue();
+                                else if (change.value instanceof Integer)
+                                    player.radius = ((Integer) change.value).floatValue();
+                                break;
                         }
 
                     }
@@ -276,15 +295,17 @@ public class Game extends ApplicationAdapter {
 
                             LinkedHashMap<String, Object> data = (LinkedHashMap<String, Object>) change.value;
 
-                            if (data.get("x") instanceof Float) fruit.x = (Float) data.get("x");
-                            else if (data.get("x") instanceof Double) fruit.x = ((Double) data.get("x")).floatValue();
+                            if (data.get("x") instanceof Float) fruit.position.x = (Float) data.get("x");
+                            else if (data.get("x") instanceof Double)
+                                fruit.position.x = ((Double) data.get("x")).floatValue();
                             else if (data.get("x") instanceof Integer)
-                                fruit.x = ((Integer) data.get("x")).floatValue();
+                                fruit.position.x = ((Integer) data.get("x")).floatValue();
 
-                            if (data.get("y") instanceof Float) fruit.y = (Float) data.get("y");
-                            else if (data.get("y") instanceof Double) fruit.y = ((Double) data.get("y")).floatValue();
+                            if (data.get("y") instanceof Float) fruit.position.y = (Float) data.get("y");
+                            else if (data.get("y") instanceof Double)
+                                fruit.position.y = ((Double) data.get("y")).floatValue();
                             else if (data.get("y") instanceof Integer)
-                                fruit.y = ((Integer) data.get("y")).floatValue();
+                                fruit.position.y = ((Integer) data.get("y")).floatValue();
 
                             int color;
                             if (data.get("color") instanceof Long) color = ((Long) data.get("color")).intValue();
