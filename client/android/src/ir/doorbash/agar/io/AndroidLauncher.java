dd662f0381c22634719e0a38fb3465dcdccdba88
Milad Doorbash <milad.doorbash@gmail.com>
2019-07-16 14:41:31 +0430

    fix android errors

--- client/android/src/ir/doorbash/agar/io/AndroidLauncher.java
+++ client/android/src/ir/doorbash/agar/io/AndroidLauncher.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.backends.android.surfaceview.ResolutionStrategy;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGdxGame(), config);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.resolutionStrategy = (widthMeasureSpec, heightMeasureSpec) -> new ResolutionStrategy.MeasuredDimension(heightMeasureSpec, heightMeasureSpec);
+        initialize(new Game(), config);
+    }
 }
